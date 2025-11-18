9e63a70e47e59108d9af805ba7c77a3369ab7995
Ivan Mozheev <mau0hezzz@hotmail.com>
2025-11-18 11:56:29 +0300

    config fix

--- vitejs/pages.config.js
+++ vitejs/pages.config.js
@@ -9,7 +9,7 @@ const pages = [
   {name: 'header', path: resolve(__dirname, '../src/scss/components/header.scss')},
   {name: 'footer', path: resolve(__dirname, '../src/scss/components/footer.scss')},
   {name: 'scroll-parallax', path: resolve(__dirname, '../src/scss/components/scroll-parallax.scss')},
-  {name: '404', path: resolve(__dirname, '../src/scss/components/404.scss')},
+  {name: '404-css', path: resolve(__dirname, '../src/scss/components/404.scss')},
 
   {name: 'header-js', path: resolve(__dirname, '../src/js/components/header.js')},
   {name: 'footer-js', path: resolve(__dirname, '../src/js/components/footer.js')},
