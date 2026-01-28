00d586f202f796d1b62da29991a549a6757c4f75
Ivan Mozheev <mau0hezzz@hotmail.com>
2026-01-28 10:57:39 +0300

    инициализация лазилоад после загрузки страницы

--- src/js/files/scroll/lazyload.js
+++ src/js/files/scroll/lazyload.js
@@ -1,11 +1,18 @@
 import LazyLoad from "vanilla-lazyload";
 
-// Работает с объектами с классом ._lazy
-const lazyMedia = new LazyLoad({
-	elements_selector: '[data-src],[data-srcset]',
-	class_loaded: '_lazy-loaded',
-	use_native: true
-});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Работает с объектами с классом ._lazy
+  const lazyMedia = new LazyLoad({
+    elements_selector: '[data-src],[data-srcset]',
+    class_loaded: '_lazy-loaded',
+    use_native: false
+  });
+
+  if (!window.mhzModules) window.mhzModules = {};
+
+  window.mhzModules.lazyMedia = lazyMedia;
+})
 
 // Обновить модуль
 //lazyMedia.update();
